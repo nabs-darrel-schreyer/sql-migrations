655382f30bb94bad990451ea49387ae8d496ecda
Darrel Schreyer <darrel.schreyer@netadvantage.co.nz>
2025-12-13 23:24:37 +1300

    Refactor migration logic into TypeExtensions
    Moved migration methods from MigrationService to TypeExtensions. Migration operations now use ProjectItem context and return success status. Added RunMigration extension method for executing migration commands.

--- src/SqlMigrations.MigrationCli/TypeExtensions.cs
+++ src/SqlMigrations.MigrationCli/TypeExtensions.cs
@@ -27,6 +27,27 @@ internal static class TypeExtensions
         }
     }
 
+    public static IReadOnlyList<MigrationOperation> GetOutstandingModelChanges<TDbContext>(this TDbContext dbContext)
+        where TDbContext : DbContext
+    {
+        var sourceSnapshot = dbContext.GetService<IMigrationsAssembly>().ModelSnapshot;
+        var runtimeInitializer = dbContext.GetService<IModelRuntimeInitializer>();
+
+        IRelationalModel? source = null;
+        if (sourceSnapshot?.Model != null)
+        {
+            var initializedModel = runtimeInitializer.Initialize(sourceSnapshot.Model, designTime: true, validationLogger: null);
+            source = initializedModel.GetRelationalModel();
+        }
+
+        var designTimeModel = dbContext.GetService<IDesignTimeModel>().Model;
+        var target = designTimeModel.GetRelationalModel();
+
+        var modelDiffer = dbContext.GetService<IMigrationsModelDiffer>();
+        var changes = modelDiffer.GetDifferences(source, target);
+        return changes;
+    }
+
     public static async Task<bool> RunMigration(this ProjectItem projectItem, string name, string command)
     {
         var projectDirectory = projectItem.ProjectFile.Directory!;
